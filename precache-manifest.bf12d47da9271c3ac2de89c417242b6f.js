50cf856e9cd24fc9819d2953c963b4e856c85c7d
Prayag C Bose <prayagcbose@gmail.com>
2020-06-18 20:59:01 +0530

    Updates

---
+++ precache-manifest.bf12d47da9271c3ac2de89c417242b6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b129d070548fb283f6753233cf04bb0",
+    "url": "/Profile/index.html"
+  },
+  {
+    "revision": "d9e793f149cf7dc88817",
+    "url": "/Profile/static/css/main.b32631f6.chunk.css"
+  },
+  {
+    "revision": "e9d82b108e626ab1ced6",
+    "url": "/Profile/static/js/2.bd5fb903.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Profile/static/js/2.bd5fb903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9e793f149cf7dc88817",
+    "url": "/Profile/static/js/main.319ad558.chunk.js"
+  },
+  {
+    "revision": "a59d21a14984125caf9d",
+    "url": "/Profile/static/js/runtime-main.5813c363.js"
+  },
+  {
+    "revision": "ae816608fb5d35ef04592a9217bacb62",
+    "url": "/Profile/static/media/pic.ae816608.jpg"
+  }
+]);
